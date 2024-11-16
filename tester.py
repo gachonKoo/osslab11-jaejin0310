bb67d18849a3a9091f6febc303c74936f29710e8
jaejin0310 <jaejin0310@gachon.ac.kr>
2024-11-16 14:37:27 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,3 +1,13 @@
+import math
+
+def calculate_hypotenuse(a, b):
+    """피타고라스 정리로 빗변 계산"""
+    return math.sqrt(a**2 + b**2)
+
+def calculate_circle_area(r):
+    """원의 면적 계산"""
+    return math.pi * r**2
+
 import geo.utils as utils
 
 a,b=3,4
