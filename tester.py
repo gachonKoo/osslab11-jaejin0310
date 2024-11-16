b182e4347e7ce930463c4bae233a9d0e79b526e6
jaejin0310 <jaejin0310@gachon.ac.kr>
2024-11-16 14:55:07 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,17 +1,9 @@
-import math
-
-def calculate_hypotenuse(a, b):
-    return math.sqrt(a**2 + b**2)
-
-def calculate_circle_area(r):
-    return math.pi * r**2
-
 import geo.utils as utils
 
 a,b=3,4
-c=utils.calculate_hypotenuse(a, b)
+c=utils.pythagoras(a, b)
 print('c=',c)
 
 r=10
-area=utils.calculate_circle_area(r) 
+area=utils.circle(r) 
 print('area=',area)
